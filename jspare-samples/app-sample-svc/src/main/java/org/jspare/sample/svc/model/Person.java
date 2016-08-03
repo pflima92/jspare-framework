b0dff78969d5ba7773cad0598d762d6437491727
paulo.lima <paulo.lima@knowtech.net.br>
2016-08-03 17:37:47 -0300

    Adjust Samples to Validation case

---
+++ jspare-samples/app-sample-svc/src/main/java/org/jspare/sample/svc/model/Person.java
@@ -0,0 +1,20 @@
+package org.jspare.sample.svc.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+
+	@NotNull
+	@Size(min = 5, max = 40)
+	private String name;
+	@CPF
+	private String cpf;
+}
